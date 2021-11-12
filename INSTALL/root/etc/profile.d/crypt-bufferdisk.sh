725efb189f9a3775fea83be6a7011aab1ac22ec0
fibroin <andi58@freenet.de>
2021-11-12 15:24:02 +0100

    bugfix open crypted bufferdisk

--- INSTALL/root/etc/profile.d/crypt-bufferdisk.sh
+++ INSTALL/root/etc/profile.d/crypt-bufferdisk.sh
@@ -1,5 +1,3 @@
-if [ -f /mnt/bufferdisk/bazaar/NOT_MOUNTED ]; then
-        echo .
-        echo "Bufferdisk is not mounted. Please enter password"
-        /opt/AmutaQ!/tools/mount_bufferdisk
-fi
+[[ $- == *i* ]] || return 0
+
+/opt/AmutaQ!/tools/mount_bufferdisk
